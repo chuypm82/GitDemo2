848da4af0a037fe712438e1cd6e02038e9f26a48
jesus <chuypm@hotmail.com>
2022-09-21 09:50:08 -0400

    second commit

--- src/WindowPopUp.java
+++ src/WindowPopUp.java
@@ -8,5 +8,11 @@ public class WindowPopUp {
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\625337\\Documents\\Personal\\Trainings\\Selenium\\Downloads\\chromedriver_win32\\chromedriver.exe");//webdriver.chrome.driver->value of path
 		WebDriver driver= new ChromeDriver();// to invoke chrome browser (launch)
 		driver.get("http://admin:admin@the-internet.herokuapp.com/basic_auth");
+		//
+		//
+		// now more steps
+		System.out.println("Test 1st line added to update 1.1 GitHub");
+		System.out.println("Test 2nd line added to update 1.1 GitHub");
+
 	}
 }
